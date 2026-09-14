c0f39384a19b6eb1fb5165423d132f2612842937
vn8690-create <vn8690@gmail.com>
2026-09-15 08:05:45 +0900

    Include Comedy Director checks in one-click tests

--- RUN_TESTS.bat
+++ RUN_TESTS.bat
@@ -14,9 +14,9 @@ echo ========================================
 echo        COMBAT CATACLYSM TESTS
 echo ========================================
 echo.
-echo [1/2] Combat Feel unit checks
-call npm run test:feel
-set FEEL_RESULT=%ERRORLEVEL%
+echo [1/2] Lightweight system checks
+call npm run test:unit
+set UNIT_RESULT=%ERRORLEVEL%
 
 echo.
 echo [2/2] Browser smoke test
@@ -24,7 +24,7 @@ call npm test
 set SMOKE_RESULT=%ERRORLEVEL%
 
 echo.
-if "%FEEL_RESULT%"=="0" if "%SMOKE_RESULT%"=="0" (
+if "%UNIT_RESULT%"=="0" if "%SMOKE_RESULT%"=="0" (
   echo ALL TESTS PASSED.
 ) else (
   echo Some tests did not pass. See the messages above.
